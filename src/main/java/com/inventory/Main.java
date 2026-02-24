210ec1cea3b44804b7015c619424704e3da53702
jmm35 <jmm35@iu.edu>
2026-02-24 15:52:31 -0500

    Add main class for Inventory System prototype

--- src/main/java/com/inventory/Main.java
+++ src/main/java/com/inventory/Main.java
@@ -1,1 +1,10 @@
+package com.inventory;
 
+
+ //Entry point of the Inventory System prototype
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inventory System Prototype Running:");
+    }
+}
